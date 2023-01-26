e9474b0be5ec27be5a4c4043e5cf04711a0189d2
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-01-26 20:14:40 +0100

    variables, shadowing

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\variables\target\debug\variables.exe: C:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\variables\src\main.rs
