0855a2737e6fce4959de3ddce6a825e3022d44df
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-02-13 22:02:56 +0100

    Added more explanation to "variables" and "Array"

---
+++ Array/src/main.rs
@@ -0,0 +1,18 @@
+/* //Array
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    let x: [u32; 5] = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+}
+*/
+
+ //Tuple
+fn main(){
+    let a = (1, 6.4, 500);
+    let x: (u8, f64, i32) = (1, 6.4, 500);
+
+    let first = a.0;
+    let second = a.1;
+}
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,10 @@
-// mutabilty
-//fn main() {
-    //let mut x = 5;
-    //println!("The value of x is: {x}");
-    //x = 6;
-    //println!("The value of x is: {x}");
-//}
+/* //mutabilty
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}*/
 
 //shadowing
 fn main() {
