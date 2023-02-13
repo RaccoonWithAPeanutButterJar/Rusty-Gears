92af46f5701be219ab2b34391ec87950274fa245
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-02-13 22:05:17 +0100

    what?

---
+++ Array/target/debug/deps/Array-3b59f42fb1f3cbf7.d
@@ -0,0 +1,5 @@
+c:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\Array\target\debug\deps\Array-3b59f42fb1f3cbf7.rmeta: src\main.rs
+
+c:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\Array\target\debug\deps\Array-3b59f42fb1f3cbf7.d: src\main.rs
+
+src\main.rs:
---
+++ Array/target/debug/deps/Array-bb4afae4f04fd137.d
@@ -0,0 +1,5 @@
+c:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\Array\target\debug\deps\Array-bb4afae4f04fd137.rmeta: src\main.rs
+
+c:\Users\Aloha\Desktop\Progemine\Git\Rusty-Gears\Array\target\debug\deps\Array-bb4afae4f04fd137.d: src\main.rs
+
+src\main.rs:
